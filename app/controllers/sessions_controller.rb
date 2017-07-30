e4be94348d4dc528cb7ecefe5672915d2aed931e
pnikrat <sniffoza@gmail.com>
2017-07-30 15:12:14 +0200

    Logging in and header layout changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # log the user in and redirect to profile page
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
