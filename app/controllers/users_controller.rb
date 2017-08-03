23488dbbe4476634b4a8c9e86c89297f1fdaffc5
pnikrat <sniffoza@gmail.com>
2017-08-03 13:22:29 +0200

    Successfule updates and friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -18,14 +21,12 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a successful update
+      flash[:success] = 'Profile updated'
+      redirect_to @user
     else
       render 'edit'
     end
@@ -36,5 +37,18 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = 'Please log in.'
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
