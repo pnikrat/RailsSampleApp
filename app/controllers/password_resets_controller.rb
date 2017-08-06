d88f20a7df3a6a94992216bd29c8dce993716610
pnikrat <sniffoza@gmail.com>
2017-08-06 12:07:52 +0200

    Password resets update action and integration tests

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update]
+
   def new; end
 
   def create
@@ -15,4 +19,43 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit; end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = 'Password has been reset'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Valid user: exists and is activated and digest corresponds to token
+    def valid_user
+      unless @user && @user.activated? &&
+             @user.authenticated?(:reset, params[:id])
+        redirect_to root_url
+      end
+    end
+
+    # expiration of reset token - check
+    def check_expiration
+      return unless @user.password_reset_expired?
+      flash[:danger] = 'Password reset has expired'
+      redirect_to new_password_reset_url
+    end
 end
